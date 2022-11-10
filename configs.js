fcff7379d567322a6488fdf377b90a6545d18c48
unknown <anders.sjunnesson@gmail.com>
2022-11-10 12:42:08 +0100

    Sessions cookie

--- configs.js
+++ configs.js
@@ -8,5 +8,7 @@ const config = dotenv.config().parsed;
 // individual exports
 const SITE_NAME = config.SITE_NAME;
 const PORT = Number(config.PORT);
+const SESSION_SECRET = config.SESSION_SECRET;
+const SESSION_MAXAGE = Number(config.SESSION_MAXAGE); 
 
-export { config, SITE_NAME, PORT };
+export { config, SITE_NAME, PORT, SESSION_SECRET, SESSION_MAXAGE };
--- server.js
+++ server.js
@@ -1,20 +1,58 @@
 // dependencies - import => package.json "type": "module",
 // ========================================
-import express from 'express';
-
+import express from "express";
+import session from "express-session";
 
 // local modules
-import { config, SITE_NAME, PORT } from './configs.js';
-
+import { config, SITE_NAME, PORT, SESSION_SECRET, SESSION_MAXAGE } from "./configs.js";
 
 // express app environment
 // ========================================
 const app = express();
 
+// sessions
+// ========================================
+app.use(
+    session({
+        secret: SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+        cookie: { maxAge: SESSION_MAXAGE },
+    })
+);
+
 
 // routes
 // ========================================
-app.get('/', (req, res) => {
+
+// check sessions
+// middleware - make sure using next as 3rd argument
+app.get('*', (req, res, next) => {
+
+    // // propety views - our own setting to hold number of times user navigates on site
+    // // condition if req.session.views not undefined
+    // if (req.session.views) {
+
+    //     // req.session.views is a number - increase
+    //     req.session.views++;
+        
+    // } else {
+        
+    //     // req.session.views is undefined - set value to 1
+    //     req.session.views = 1;
+    // }
+
+    // oneliner if condition - ternary operator    ? :  ;
+    req.session.views ? req.session.views++ : req.session.views = 1;
+    
+    // show number of times users navigates before session been destroyed
+    console.log("req.session.views", req.session.views);
+
+    next();
+});
+
+
+app.get("/", (req, res) => {
     res.send(`Hello world ${config.SITE_NAME}`);
 });
 
