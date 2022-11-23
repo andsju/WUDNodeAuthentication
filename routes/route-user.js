c13b9713519050f7d24947ecadd2bc1b7e36b219
unknown <anders.sjunnesson@gmail.com>
2022-11-23 09:43:56 +0100

    Initial handle user

--- routes/route-user.js
+++ routes/route-user.js
@@ -7,10 +7,13 @@ import { listUsers } from '../controllers/controller-user.js';
 
 // send json response
 router.get("/", (req, res) => {
-    listUsers().then(data => {
-        console.log("data", data);
-        res.json(data);    
-    });
+
+    res.render("user", {site: SITE_NAME});
+
+    // listUsers().then(data => {
+    //     console.log("data", data);
+    //     res.json(data);    
+    // });
 });
 
 export default router;
